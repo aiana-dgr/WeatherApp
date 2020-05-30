58f7da853fbbe9ee7351a09e9b7df41e5910a744
aiana <rubio.aiana@gmail.com>
2020-05-31 04:34:37 +0800

    Stored an actual reference to the activity with a WeakReference on GetTemp ans GetForecast Runnable classes

--- app/src/main/java/com/example/weatherapp/GetForecast.java
+++ app/src/main/java/com/example/weatherapp/GetForecast.java
@@ -1,30 +1,38 @@
 package com.example.weatherapp;
 
-import android.util.Log;
+
+import java.lang.ref.WeakReference;
 
 public class GetForecast implements Runnable {
-    private MainActivity activity;
+    private WeakReference<MainActivity> activityRef;
     private String city;
 
     /**
      * Sets up the runnable to be called. It needs the MainActivity so it can run code on the
      * UI thread, and also the city so that it can get its weather conditions.
+     *
      * @param activity
      * @param city
      */
     public GetForecast(MainActivity activity, String city) {
-        this.activity = activity;
+        this.activityRef = new WeakReference<MainActivity>(activity);
         this.city = city;
     }
+
     @Override
     public void run() {
+        // Get the activity from the WeakReference
+        final MainActivity activity = activityRef.get();
+
         // This is the function that will be run on the background thread.
         WeatherDataLoader loader = new WeatherDataLoader();
-        loader.getForecastAndPostResults(city, (forecast) -> {
-            activity.runOnUiThread(() -> {
-                // forecast contains the results of the API call
-                activity.handleWeatherForecastResult(forecast);
+        if (activity != null) {
+            loader.getForecastAndPostResults(city, (forecast) -> {
+                activity.runOnUiThread(() -> {
+                    // forecast contains the results of the API call
+                    activity.handleWeatherForecastResult(forecast);
+                });
             });
-        });
+        }
     }
 }
--- app/src/main/java/com/example/weatherapp/GetTemperature.java
+++ app/src/main/java/com/example/weatherapp/GetTemperature.java
@@ -2,20 +2,24 @@ package com.example.weatherapp;
 
 import android.util.Log;
 
+import java.lang.ref.WeakReference;
+
 public class GetTemperature implements Runnable {
-    private MainActivity activity;
+    private WeakReference<MainActivity> activityRef;
     private String city;
 
     /**
      * Sets up the runnable to be called. It needs the MainActivity so it can run code on the
      * UI thread, and also the city so that it can get its weather conditions.
+     *
      * @param activity
      * @param city
      */
     public GetTemperature(MainActivity activity, String city) {
-        this.activity = activity;
+        this.activityRef = new WeakReference<MainActivity>(activity);
         this.city = city;
     }
+
     @Override
     public void run() {
         // This is the function that will be run on the background thread.
@@ -26,13 +30,20 @@ public class GetTemperature implements Runnable {
         // object that we are giving it.
 
         final WeatherConditions conditions = loader.getWeatherAndPostResults(city);
-        activity.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                // This is code that will now run on the UI thread. Call the function in
-                // MainActivity that will update the UI correctly.
-                activity.handleWeatherConditionsResult(conditions);
-            }
-        });
+
+        // Get the activity from the WeakReference
+        final MainActivity activity = activityRef.get();
+
+        // Check that activity is not destroyed
+        if (activity != null) {
+            activity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    // This is code that will now run on the UI thread. Call the function in
+                    // MainActivity that will update the UI correctly.
+                    activity.handleWeatherConditionsResult(conditions);
+                }
+            });
+        }
     }
 }
